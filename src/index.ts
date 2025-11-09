ef501b5de8ec3bd82a59226ab057812254985574
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2025-11-09 23:43:43 +0000

    minecraft

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import { VirtualWindow } from "ollieos/src/windowing";
+import type { Program } from "ollieos/src/types";
+
+export default {
+    name: "minecraft",
+    description: "Minecraft.",
+    usage_suffix: "",
+    arg_descriptions: {},
+    main: async (data) => {
+        const iframe = document.createElement("iframe");
+        iframe.src = "https://classic.minecraft.net/";
+        iframe.style.border = "none";
+        iframe.style.width = "100%";
+        iframe.style.height = "100%";
+
+        const wind = new VirtualWindow();
+        wind.title = "Minecraft";
+
+        const width = 75;
+        const height = width * 4/3;
+
+        const x = (100 - width) / 2;
+        const y = (100 - height) / 2;
+
+        wind.width = `${width}vw`;
+        wind.height = `${height}vh`;
+
+        wind.x = `${x}vw`;
+        wind.y = `${y}vh`;
+
+        wind.dom.appendChild(iframe);
+        wind.show();
+
+        return 0;
+    }
+} as Program;
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+/* eslint-env node */
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const pkgbuild = require("ollieos_pkgbuild");
+
+// EDIT THIS OBJECT TO ADD MORE PROGRAMS OR CHANGE THE FILE PATHS/NAMES
+// key: the name of the program
+// value: the path to the entry point
+const programs = {
+    "rallylife": "./src/index.ts",
+};
+
+// EDIT THIS ARRAY TO ADD DEPENDENCIES FOR THE VERSION CURRENTLY BEING BUILT
+// format: name@version
+const deps = [];
+
+// EDIT THIS TO CHANGE THE HOMEPAGE URL
+const homepage_url = "https://ollieg.codes";
+
+
+// EDIT THIS OBJECT TO DEFINE ADDITIONAL WEBPACK EXTERNALS
+// key: the name of the module
+// value: the external name
+const externals = {};
+
+module.exports = pkgbuild(programs, deps, homepage_url, externals);
